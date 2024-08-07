e698c4706b1514cff82d221ab92b4129e3d9db77
Jakob Meyer <jakobmeyer.feu@gmail.com>
2024-08-08 00:51:45 +0200

    add gltf model

--- src/three_background.js
+++ src/three_background.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { loadModel } from './loadModel'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 // renderer
 const canvas = document.querySelector("canvas.webgl")
@@ -16,23 +16,24 @@ const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x333333)
 
 // camera
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 9000)
 camera.position.set(7, 2, 2)
 scene.add(camera)
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.minDistance = 5
 controls.maxDistance = 15
 controls.enablePan = false
-controls.maxPolarAngle = Math.PI / 2 - 0.05
+// controls.maxPolarAngle = Math.PI / 2 - 0.05
 
 // light
 const pointLight = new THREE.PointLight(0xffffff, 10)
 pointLight.position.set(0, 0, 2)
 scene.add(pointLight)
 const ambientLight = new THREE.AmbientLight(0xffffff)
+ambientLight.intensity = 2
 scene.add(ambientLight)
-const lightHelper = new THREE.PointLightHelper(pointLight, 0.1)
-scene.add(lightHelper)
+// const lightHelper = new THREE.PointLightHelper(pointLight, 0.1)
+// scene.add(lightHelper)
 
 // grid
 const gridHelper = new THREE.GridHelper()
@@ -44,16 +45,52 @@ const material = new THREE.MeshPhongMaterial({ color: 0x00cc00 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// cursor check
+const cursor = { x: 0, y: 0 }
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / window.innerWidth - 0.5
+    cursor.y = event.clientY / window.innerHeight - 0.5
+})
+
 // model
-// loadModel()
+const loader = new GLTFLoader();
+loader.load(
+    // resource URL
+    "public/Electric_Towers.gltf",
+    // called when the resource is loaded
+    function (gltf) {
+        // add the whole scene (10 objects here)
+        // scene.add(gltf.scene);
+        // select one object instead of the whole scene
+        let model = gltf.scene.getObjectByName("Tower_8").clone();
+        model.position.set(0, 0, 0);
+
+        const y = () => {
+            window.requestAnimationFrame(y)
+            model.rotation.x = cursor.y / 4
+            model.rotation.y = cursor.x / 4
+        }
+        y()
+
+        scene.add(model);
+    },
+    // called while loading is progressing
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    // called when loading has errors
+    function (error) {
+        console.error(error);
+    }
+)
 
 // render everything
 function animate() {
     requestAnimationFrame(animate)
-
+    // rotate objects
     mesh.rotation.x += 0.01
     mesh.rotation.y += 0.01
-
+    // update and render
     controls.update()
     renderer.render(scene, camera)
 }
@@ -61,9 +98,8 @@ animate()
 
 // resize handler
 function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    keyDisplayQueue.updatePosition()
 }
 window.addEventListener('resize', onWindowResize);
